4638fc0f1e9380b09567e10b2be2f5d91a59c57c
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-04-13 16:51:56 -0700

    .

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-// const routes = require('./routes/index.js');
+const routes = require('./routes/index.js');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(port, (error) => {
   if (error) {
     console.error(error.message);
   } else {
-    console.log('APP Started Successfully');
+    console.log(`Server is running on port: ${port}`);
   }
 });
-
-// app.use('/', routes);
