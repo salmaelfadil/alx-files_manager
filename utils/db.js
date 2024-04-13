f73a646a1df3e1697c71462ec9ecede6d9c3fb6a
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-04-13 16:15:45 -0700

    fix eslint

--- utils/db.js
+++ utils/db.js
@@ -1,26 +1,27 @@
 const { MongoClient } = require('mongodb');
 
 class DBClient {
-	constructor() {
-		const host = process.env.DB_HOST || 'localhost';
-		const port = process.env.DB_PORT || 27017;
-		const database = process.env.DB_DATABASE || 'files_manager';
-		const URL = `mongodb://${host}:${port}/${database}`;
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const URL = `mongodb://${host}:${port}/${database}`;
 
-		this.client = new MongoClient(URL);
-		this.client.connect().catch((error) => console.log(error.message));
-	}
-	isAlive() {
-		return this.client.isConnected();
-	}
+    this.client = new MongoClient(URL);
+    this.client.connect().catch((error) => console.log(error.message));
+  }
 
-	async nbUsers() {
-		return this.client.db().collection('users').countDocuments();
-	}
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-	async nbFiles() {
-		return this.client.db().collection('files').countDocuments();
-	}
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
 }
 const dbClient = new DBClient();
 module.exports = dbClient;
