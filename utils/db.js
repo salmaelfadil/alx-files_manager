cd6bfb8e3fac683319dcbbf9a111bafc0432ebd3
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-04-13 16:13:05 -0700

    .

--- utils/db.js
+++ utils/db.js
@@ -21,3 +21,6 @@ class DBClient {
 	async nbFiles() {
 		return this.client.db().collection('files').countDocuments();
 	}
+}
+const dbClient = new DBClient();
+module.exports = dbClient;
--- utils/redis.js
+++ utils/redis.js
@@ -2,30 +2,30 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-	constructor() {
-		this.client = createClient();
-		this.client.on('error', (err) => {
-			console.log(err);
-			this.connected = false;
-		});
-		this.connected = true;
-	}
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log(err);
+      this.connected = false;
+    });
+    this.connected = true;
+  }
 
-	isAlive() {
-		return this.connected;
-	}
+  isAlive() {
+    return this.connected;
+  }
 
-	async get(key) {
-		return promisify(this.client.get).bind(this.client)(key);
-	}
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
 
-	async set(key, value, dur) {
-		await promisify(this.client.setex).bind(this.client)(key, dur, value);
-	}
-	
-	async del(key) {
-		await promisify(this.client.del).bind(this.client)(key);
-	}
+  async set(key, value, dur) {
+    await promisify(this.client.setex).bind(this.client)(key, dur, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
 }
 
 const redisClient = new RedisClient();
