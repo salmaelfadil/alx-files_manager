5233782e77737dc574ba67ddaaec8d705e55922c
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-04-14 15:34:38 -0700

    ..

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,29 +4,29 @@ const dbClient = require('../utils/db');
 class userController {
   static async postNew(req, res) {
     try {
-	    const { email } = req.body;
-	    if (!email) {
-		    res.status(400).json({ error: 'Missing email' });
-		    return;
-	    }
-	    const { password } = req.body;
-	    if (!password) {
-		    res.status(400).json({ error: 'Missing password' });
-		    return;
-	    }
-	    const user = await (await dbClient.usersCollection()).findOne({ email });
-	    if (user) {
-		    res.status(400).json({ error: 'Already exist' });
-		    return;
-	    }
-	    const hashPass = sha1(password);
-	    const new_user = await dbClient.createUser(email, hashPass);
-	    const id = new_user.insertedId;
-	    res.status(201).json({ id, email });
+      const { email } = req.body;
+      if (!email) {
+        res.status(400).json({ error: 'Missing email' });
+        return;
+      }
+      const { password } = req.body;
+      if (!password) {
+        res.status(400).json({ error: 'Missing password' });
+        return;
+      }
+      const user = await (await dbClient.usersCollection()).findOne({ email });
+      if (user) {
+        res.status(400).json({ error: 'Already exist' });
+        return;
+      }
+      const hashPass = sha1(password);
+      const newUser = await dbClient.createUser(email, hashPass);
+      const id = newUser.insertedId;
+      res.status(201).json({ id, email });
     } catch (error) {
-	  console.error(error.message);
-	  res.status(500).json({ error: 'Internal server error' });
+      console.error(error.message);
+      res.status(500).json({ error: 'Internal server error' });
     }
-}
+  }
 }
 module.exports = userController;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes/index.js');
+const routes = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
