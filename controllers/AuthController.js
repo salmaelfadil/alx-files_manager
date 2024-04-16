7363c1aaff9ff08c140c0f48992de3e80f2a2277
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-04-16 16:31:05 -0700

    .

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,24 +5,19 @@ import redisClient from '../utils/redis';
 
 class AuthController {
   static async getConnect(req, res) {
-    if (!req.headers.authorization) {
-      res.status(401);
-      res.send('Missing Authorization');
+    const cred = req.header('Authorization').split(' ')[1];
+    const decodedCred = Buffer.from(cred, 'base64').toString('ascii');
+    const email = decodedCred.split(':')[0];
+    const hashPass = sha1(decodedCred.split(':')[1]);
+    const user = await (await dbClient.usersCollection()).findOne({ email, password: hashPass });
+    if (!user) {
+      res.status(401).json({ error: 'Unauthorized' });
     } else {
-      const cred = req.header('Authorization').split(' ')[1];
-      const decodedCred = Buffer.from(cred, 'base64').toString('ascii');
-      const email = decodedCred.split(':')[0];
-      const hashPass = sha1(decodedCred.split(':')[1]);
-      const user = await (await dbClient.usersCollection()).findOne({ email, password: hashPass });
-      if (!user) {
-        res.status(401).json({ error: 'Unauthorized' });
-      } else {
-        const token = uuidv4();
-        const userId = user._id.toString();
-        // console.log('User ID:', userId);
-        await redisClient.set(`auth_${token}`, userId, 60 * 60 * 24);
-        res.status(200).json({ token });
-      }
+      const token = uuidv4();
+      const userId = user._id.toString();
+      // console.log('User ID:', userId);
+      await redisClient.set(`auth_${token}`, userId, 60 * 60 * 24);
+      res.status(200).json({ token });
     }
   }
 
--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -10,9 +10,29 @@ class FilesController {
 		} else {
 			const name = req.body.name;
 			if (!name) {
-				res.status(400).json({ 'error': 'Missing Type' });
+				res.status(400).json({ 'error': 'Missing name' });
 			}
 			const type = req.body.type;
+			const typeList = [folder, file, image];
+			if (!type || !typeList.includes(type)) {
+				res.status(400).json({ 'error': 'Missing type' });
+			}
+			if (type === file ||  type === image) {
+				const data = req.body.data;
+				if (!data) {
+					res.status(400).json({ 'error': 'Missing data' });
+				}
+			}
+			const parentId = req.body.parentId;
+			if (parentId) {
+				parentIdObj = new ObjectID(parentId);
+				parentExists = await (await.dbClient.filesCollection()).findOne({ _id: parentIdObj });
+				if (!parentExists) {
+					res.status(400).json({ 'error': 'parent not found' });
+				} else if (type(parentExists) != folder) {
+					res.status(400).json({ 'error': 'Parent is not a folder' });
+				}
+			}
 
 		}
 	}
