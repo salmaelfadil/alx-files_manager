bf2dbcff976fa10aaa70fc9d0cde5d3cbfbbf071
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-04-17 11:37:00 -0700

    .

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UserController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-// const FilesController = require('../controllers/FilesController.js');
+const FilesController = require('../controllers/FilesController.js');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -12,6 +12,6 @@ router.post('/users', UserController.postNew);
 router.get('/users/me', UserController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-// router.post('/files', FilesController.postUpload);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
